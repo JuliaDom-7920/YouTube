19480b9a27e931069b84a946eb59b0686f3fa696
JuliaDom-7920 <warrior021325@gmail.com>
2021-04-11 15:54:30 +0300

    Edit js file for info-channel.html

--- js/info-channel.js
+++ js/info-channel.js
@@ -1,3 +1,42 @@
+"user strict";
+
+//получаєм переключатель теми
+const switchThemeElem = document.querySelector(".switch");
+
+//установлення теми після загрузкі сторінкі
+function setLocalTheme() {
+  if (JSON.parse(localStorage.getItem("theme"))) {
+    //для боді добавляю класс, щоб можна було управляти в стилях і js якщо треба буде
+    document.body.classList.add("theme-toggle");
+  }
+}
+setLocalTheme(localStorage.getItem("theme"));
+
+function handlerThemeInstall(theme) {
+  if (!theme) {
+    switchThemeElem.classList.add("_dark");
+  }
+}
+handlerThemeInstall(localStorage.getItem("theme"));
+
+switchThemeElem.addEventListener("change", () => {
+  if (document.body.classList.contains("theme-toggle")) {
+    document.body.classList.remove("theme-toggle");
+    changeImgPreloaderTheme(false);
+    console.log(true);
+    localStorage.removeItem("theme");
+    switchThemeElem.classList.remove("_dark");
+  } else {
+    document.body.classList.add("theme-toggle");
+    localStorage.setItem("theme", true);
+    changeImgPreloaderTheme(true);
+  }
+});
+
+function formatingNumber(number) {
+  return Number(number).toLocaleString({ style: "percent" });
+}
+
 // Глобальна інформація про канал ========================================================
 
 let data;
@@ -122,6 +161,3 @@ function renderHTML() {
 }
 
 document.querySelector(".search__btn").addEventListener("click", renderHTML);
-
-// Порівняння інформації про канал ========================================================
-
