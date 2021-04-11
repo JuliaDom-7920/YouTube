088bc2a43a6ec09ffc266076e1d3605a1dc1ffae
JuliaDom-7920 <warrior021325@gmail.com>
2021-04-11 18:55:42 +0300

    Add modal window

--- js/index.js
+++ js/index.js
@@ -99,3 +99,219 @@ switchThemeElem.addEventListener("change", () => {
 function formatingNumber(number) {
   return Number(number).toLocaleString({ style: "percent" });
 }
+
+// Модальне вікно додавання каналу
+class Modal {
+  constructor(opts = {}) {
+    this.targetElem = opts.targetElem;
+    this.selectorOut = opts.selectorOut;
+    this.html = opts.html;
+  }
+
+  init() {
+    console.log(this);
+    document
+      .querySelector(this.targetElem)
+      .addEventListener("click", this.create);
+  }
+
+  create() {
+    const afterElem = document.querySelector(this.selectorOut);
+    const modalElem = document.createElement("div");
+    const overlayElem = document.createElement("div");
+    const bodyElem = document.createElement("div");
+    const closeBtnElem = document.createElement("button");
+
+    closeBtnElem.innerText = "×";
+
+    //document.body.classList.add("lock");
+
+    modalElem.classList.add("modal");
+    overlayElem.classList.add("modal__overlay");
+    bodyElem.classList.add("modal__body");
+    closeBtnElem.classList.add("modal__close");
+
+    afterElem.insertAdjacentElement("afterbegin", modalElem);
+    modalElem.append(overlayElem);
+    modalElem.append(bodyElem);
+    bodyElem.append(closeBtnElem);
+    bodyElem.innerHTML += this.html;
+
+    document
+      .querySelector(".modal__close")
+      .addEventListener("click", this.close);
+
+    document.querySelector(".modal__overlay").addEventListener("click", () => {
+      document.querySelector(".modal").remove();
+    });
+
+    //====================================================================================
+    let data;
+
+    function loadClient() {
+      gapi.client.setApiKey("AIzaSyCMQPA4JFFHpFPK-sr00Z6YRHeyJr-3_iA");
+      return gapi.client
+        .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(
+          function () {
+            console.log("ok");
+          },
+          function (err) {
+            console.error("Error loading GAPI client for API", err);
+          }
+        );
+    }
+
+    function execute() {
+      loadClient();
+      return gapi.client.youtube.channels
+        .list({
+          part: ["snippet,contentDetails,statistics"],
+          // id: ["UC_x5XG1OV2P6uZZ5FSM9Ttw"],
+          id: [document.querySelector(".input-card").value],
+        })
+        .then(
+          function (response) {
+            data = JSON.parse(response.body);
+          },
+          function (err) {
+            console.error("Execute error", err);
+          }
+        );
+    }
+    gapi.load("client:auth2");
+
+    //====================================================================================
+    let cards = [];
+
+    if (localStorage.getItem("cards")) {
+      cards = JSON.parse(localStorage.getItem("cards"));
+    }
+
+    document.querySelector(".add-card").addEventListener("click", () => {
+      cards.push(document.querySelector(".input-card").value);
+      localStorage.setItem("cards", JSON.stringify(cards));
+      console.log(cards);
+    });
+  }
+
+  close() {
+    document.querySelector(".modal").remove();
+  }
+
+}
+
+// example for created modal
+const addBtnElem = document.querySelector(".header__add");
+
+addBtnElem.addEventListener("click", () => {
+  new Modal({
+    targetElem: ".header__add",
+    selectorOut: ".card-channel__body",
+    html: htmlToModal(),
+  }).create();
+});
+
+function htmlToModal() {
+  return `<input class='input-card' type='text' placeholder='Ведіть id канала' /><button class='add-card btn'>Додати</button>`;
+}
+
+//====================================================================================
+//channels insert
+function insert() {
+  if (localStorage.getItem("cards")) {
+  
+    const main = document.querySelector(".card-channel__body");
+    
+    document.querySelector(".hello").remove();
+    document.querySelector(".card-channel").style.display = "block";
+      
+    JSON.parse(localStorage.getItem("cards")).forEach((card, index) => {
+      main.innerHTML += 
+        `
+          <div class="card-channel__block" data-id="${index}">
+            <button class="card-channel__close" data-id="${index}"></button>
+            <div class="card-channel__icon">Icon</div>
+            <div class="card-channel__title">Title</div>
+            <div class="card-channel__btns">
+              <button class="btn">Детальніше</button>
+            </div>
+          </div>
+        `;
+      //body for insert ${card}
+      //<div class="card-channel__icon">${data.items[0].snippet.thumbnails.default.url}</div>
+      //<div class="card-channel__title">${data.items[0].snippet.localized.title}</div>
+    });
+  }
+}
+
+
+//channels delete
+const closeBtn = document.querySelectorAll(".card-channel__close");
+closeBtn.forEach((btn => { 
+  btn.addEventListener("click", (event) => {
+    
+    const target = event.target;
+    if(localStorage.getItem("cards")) {
+      if(target.classList.contains("card-channel__close")) {
+
+        const id = target.getAttribute("data-id");
+
+        let cards = JSON.parse(localStorage.getItem("cards")).splice(1,id);
+
+        localStorage.setItem(cards,JSON.stringify(cards));
+        console.log(cards);
+        insert();
+      }
+    }
+  });
+}));
+
+
+
+window.onload = function loadClient() {
+  gapi.client.setApiKey("AIzaSyCMQPA4JFFHpFPK-sr00Z6YRHeyJr-3_iA");
+  return gapi.client
+    .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+    .then(
+      function () {
+       console.log('ok')
+      },
+      function (err) {
+        console.error("Error loading GAPI client for API", err);
+      }
+    );
+};
+
+function getData() {
+  let data ;
+ function loadClient() {
+    gapi.client.setApiKey("AIzaSyCMQPA4JFFHpFPK-sr00Z6YRHeyJr-3_iA");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute() {
+    return gapi.client.youtube.channels.list({
+      "part": [
+        "snippet,contentDetails,statistics"
+      ],
+      "id": [
+        "UC_x5XG1OV2P6uZZ5FSM9Ttw"
+      ]
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                data =  {
+                  title: response.result.items[0].snippet.title,
+                  img: response.result.items[0].snippet.thumbnails.default.url,
+                }
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  gapi.load("client:auth2");
+  loadClient();
+  execute();
+  return data;
+}
